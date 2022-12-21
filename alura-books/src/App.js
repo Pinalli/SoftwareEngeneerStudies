56567f95fa2c86292151087ad13a9a5fca8e5452
Alberto Rocha Pinalli <betopinalli@gmail.com>
2022-12-21 14:28:33 -0300

    search component

--- alura-books/src/App.js
+++ alura-books/src/App.js
@@ -1,19 +1,19 @@
 
 import Header from './components/Header';
 import styled from 'styled-components';
+import Search from './components/Search';
 
 const AppContainer = styled.div`
    
     width: 100vw;
     height: 100vh;
-    background-image: linear-gradient(90deg, #002F52 35%, #326589); 
-  
+    background-image: linear-gradient(90deg, #002F52 35%, #326589);   
 `
-
 function App() {
   return (
     <AppContainer >
       <Header />
+      <Search />
     </AppContainer >
   );
 }
---
+++ alura-books/src/components/Search/index.js
@@ -0,0 +1,34 @@
+import Input from '../Input'
+import styled from 'styled-components'
+
+const SearchContainer = styled.section`
+    background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);   
+    color: #FFF;
+    text-align: center;
+    padding: 85px 0;
+    height:  270px;
+    width: 100%;
+`
+const Title = styled.h2`
+    color: #FFF;
+    font-size: 16px;
+    text-align: center;
+    width: 100%;
+`
+const SubTitle = styled.h3`
+    font-size: 16px;
+    font-weight: 500;
+    margin-bottom: 40px;
+`
+
+function Search() {
+    return (
+        <SearchContainer>
+            <Title>Já sabe por onde começar?</Title>
+            <SubTitle>Encontre seu livro em nossa estante</SubTitle>
+            <Input placeholder="Digite o nome do livro" />
+        </SearchContainer>
+    )
+}
+
+export default Search
