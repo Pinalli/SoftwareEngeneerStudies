be32b2d2b51cc7e03c797afc740a7b4a45aaf9e9
Alberto Rocha Pinalli <betopinalli@gmail.com>
2022-12-29 14:37:17 -0300

    promises and regex

--- node-lib-md/index.js
+++ node-lib-md/index.js
@@ -1,10 +1,52 @@
+//File System
 import fs from 'fs'
 import chalk from 'chalk';
 
-function getFile(pathFile) {
-    const encoding = 'utf8';
-    fs.readFile(pathFile, encoding, (_, data) => {
-        console.log(chalk.green(data));
-    })
+function treatError(error) {
+    console.log(error)
+    throw new Error(chalk.red(error.code, 'No such file or directory'));
 }
+
+// async await
+async function getFile(pathFile) {
+    try {
+        const encoding = 'utf8';
+        const data = await fs.promises
+            .readFile(pathFile, encoding);
+        console.log(chalk.green(data));
+    }
+    catch (error) {
+        treatError(error);
+    }
+}
+
 getFile('./files/texto.md');
+//getFile('./files/');
+
+//\[[^[\]]*?] //regex to find links
+//\(https?:\/\/[^\s?#.].[^\s]*\) 
+
+// promises with .then
+// function getFile(pathFile) {
+//     const encoding = 'utf8';
+
+//     fs.promisses
+//         .readFile(pathFile, encoding) //return a promise
+//         .then((data) => console.log(chalk.green(data)))//then is a promise
+//         .catch(treatError)           //or  .catch((error) => treatError(error));
+// }
+
+
+// function getFile(pathFile) {
+//     const encoding = 'utf8';
+//     fs.readFile(pathFile, encoding, (error, data) => {
+//         if (error) {
+//             treatError(error);
+//         }
+//         console.log(chalk.green(data));
+//     })
+// }
+
+
+
+
