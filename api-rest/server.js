357c240cd5c354f5a19451ab0684e4745b30134b
Alberto Rocha Pinalli <betopinalli@gmail.com>
2023-01-02 19:39:50 -0300

    crud http

--- api-rest/server.js
+++ api-rest/server.js
@@ -1,17 +1,7 @@
-const http = require('http');
-const port = 3000;
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end(routs[req.url]);
-});
+import app from './src/app.js';
 
-const routs = {
-    '/': "Node course",
-    '/books': 'Node.js book',
-    '/author': 'Authors list',
-    '/about': 'Info about the course'
-}
+const port = process.env.PORT || 3000;
 
-server.listen(port, () => {
+app.listen(port, () => {
     console.log(`Server running in http://localhost:${port}`);
 });
--- api-rest/src/app.js
+++ api-rest/src/app.js
@@ -1,18 +1,54 @@
-import express from 'express';
+import express from "express"
 
 const app = express();
 
+app.use(express.json());
+
 const books = [
-    { id: 1, title: 'The Lord of the Rings' },
-    { id: 2, title: 'The Hobbit' }
+    { id: 1, title: "Harry Potter", author: "J.K. Rowling" },
+    { id: 2, title: "The Lord of the Rings", author: "J.R.R. Tolkien" },
+    { id: 3, title: "The Hobbit", author: "J.R.R. Tolkien" },
+    { id: 4, title: "The Chronicles of Narnia", author: "C.S. Lewis" },
+    { id: 5, title: "The Lion, the Witch and the Wardrobe", author: "C.S. Lewis" },
+    { id: 6, title: "The Da Vinci Code", author: "Dan Brown" },
+    { id: 7, title: "The Alchemist", author: "Paulo Coelho" }
 ]
 
-app.get('/', (req, res) => {
-    res.status(200).send('Course of the Node');
+app.get("/", (req, res) => {
+    res.status(200).send("Node Course");
 })
 
-app.get('/books', (req, res) => {
+app.get("/books", (req, res) => {
     res.status(200).json(books);
 })
 
-export default app
+app.get('/books/:id', (req, res) => {
+    const index = getBooks(req.params.id);
+    res.json(books[index]);
+})
+
+
+app.post('/books', (req, res) => {
+    const book = req.body;
+    books.push(book);
+    res.status(201).send('book has added successfully');
+})
+
+app.put('/books/:id', (req, res) => {
+    const index = getBooks(req.params.id);
+    books[index].title = req.body.title;
+    res.status(200).json(books);
+})
+
+function getBooks(id) {
+    return books.findIndex(book => book.id == id);
+}
+
+app.delete('/books/:id', (req, res) => {
+    let { id } = req.params;
+    let index = getBooks(req.params.id);
+    books.splice(index, 1);
+    res.send(`Books ${id} has been deleted`);
+})
+
+export default app;
