9f8145a01fdb1afb172673c96909a4224985d454
Alberto Rocha Pinalli <betopinalli@gmail.com>
2023-01-05 13:19:44 -0300

    js_poo

---
+++ js-poo/literal-object.js
@@ -0,0 +1,12 @@
+const user = {
+    name: 'John',
+    email: 'john@email.com',
+    bornDate: '1990/01/01',
+    role: 'admin',
+    active: true,
+    displayInfo: function () {
+        console.log(`Name: ${this.name} - Email: ${this.email}`);
+    }
+
+}
+
